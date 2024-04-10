b4a3ca7824ca69da11aa87cc8a37bed6b1cc22c6
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-10 11:02:55 +0200

    Buscar la palabra con strPos en lugar de contains

--- e-1/index.php
+++ e-1/index.php
@@ -3,7 +3,7 @@ $algas = "Comer algas es realmente sano";
 echo $algas . '<br>';
 echo (strpos($algas, "algas") + 1) . '<br>';
 echo str_replace(['Comer', 'realmente'], 'muy', $algas) . '<br>';
-if (str_contains($algas, "anacardo")){
+if (strpos($algas, "anacardo") !== false){
     echo 'true' . "<br>";
 }else{
     echo 'false' . "<br>";
