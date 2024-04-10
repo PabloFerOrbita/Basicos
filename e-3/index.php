7bb7f81c7cac05a981cdd4b28760503e0190f63a
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-10 12:00:49 +0200

    convertir cadena en array separado por -

--- e-3/index.php
+++ e-3/index.php
@@ -1,4 +1,6 @@
 <?php
 $cadena = "a-a-r-e-f-a";
-echo str_replace('a', 'tortuga', $cadena);
+echo str_replace('a', 'tortuga', $cadena) . '<br>';
+$array = explode('-', $cadena);
+echo print_r($array, true);
 ?>
