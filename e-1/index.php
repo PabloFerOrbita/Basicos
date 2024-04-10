01264e05b15a72e51960aa9880f66b45df8a0b92
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-10 11:00:31 +0200

    Reemplazar Comer y realmente por muy

--- e-1/index.php
+++ e-1/index.php
@@ -1,3 +1,14 @@
 <?php
+$algas = "Comer algas es realmente sano";
+echo $algas . '<br>';
+echo (strpos($algas, "algas") + 1) . '<br>';
+echo str_replace(['Comer', 'realmente'], 'muy', $algas) . '<br>';
+if (str_contains($algas, "anacardo")){
+    echo 'true' . "<br>";
+}else{
+    echo 'false' . "<br>";
+}
+echo strrev($algas) . '<br>';
+echo substr_count($algas, 'e');
 
 ?>
