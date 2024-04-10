d10fa6a4eb1a31b058cfd0ef17b0ca49268fa44d
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-10 11:40:19 +0200

    Calcular raiz y mostrarla sin decimales

--- e-2/index.php
+++ e-2/index.php
@@ -6,9 +6,11 @@ if(15 % 3 == 0){
     echo 'false' . '<br>';
 }
 $valorMul = $valor * 21;
-echo $valorMul;
+echo $valorMul . '<br>';
 for($i = 0; $i < 100; $i++){
     $valorMul = $valorMul + 1;
 }
-echo $valorMul;
+echo $valorMul . '<br>';
+$valorRaiz = sqrt($valorMul);
+echo number_format($valorRaiz, 0);
 ?>
