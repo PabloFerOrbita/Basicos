45bd6970bdfacd0107272266f43be839ceca56ba
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-10 12:07:15 +0200

    ordenar array alfabéticamente de manera descendente

--- e-3/index.php
+++ e-3/index.php
@@ -2,5 +2,7 @@
 $cadena = "a-a-r-e-f-a";
 echo str_replace('a', 'tortuga', $cadena) . '<br>';
 $array = explode('-', $cadena);
-echo print_r($array, true);
+echo print_r($array, true) . '<br>';
+rsort($array);
+echo print_r($array, true) . '<br>';
 ?>
