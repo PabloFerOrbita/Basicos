e4e3537ac10fbf35a1620952225773a73dab37cf
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-10 12:15:50 +0200

    array filtrando los valores que contienen una a

--- e-3/index.php
+++ e-3/index.php
@@ -1,8 +1,12 @@
 <?php
 $cadena = "a-a-r-e-f-a";
-echo str_replace('a', 'tortuga', $cadena) . '<br>';
+echo $cadena . '<br>';
+$cadena = str_replace('a', 'tortuga', $cadena);
+echo $cadena  . '<br>';
 $array = explode('-', $cadena);
 echo print_r($array, true) . '<br>';
 rsort($array);
 echo print_r($array, true) . '<br>';
+$arrayFiltrado = array_filter($array, function($v){ return !str_contains($v, 'a');});
+echo print_r($arrayFiltrado, true) . '<br>';
 ?>
