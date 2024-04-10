f537fa65059f8da4301d877fbffab7ffc917e2f9
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-10 12:26:03 +0200

    valores modificados

--- e-4/index.php
+++ e-4/index.php
@@ -1,3 +1,8 @@
 <?php
 $array = array(11, 12, 13, 14, 15, 16, 17);
+echo print_r($array, true) . '<br>';
+foreach($array as $clave => $valor){
+    $array[$clave] = $valor + 5;
+}
+echo print_r($array, true);
 ?>
